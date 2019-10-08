a67924890b61d399937ed1e255bf8ca7bdef69f9
C Y Law <chevy.cy.law@gmail.com>
2019-10-07 21:53:41 -0700

    Test upload 2

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Logo from '../image/bunnyLogo.png';
-
-import Background4 from '../image/photoBackgroundImage.JPG';
 import Sidebar1 from '../image/sidebar1.png';
 import Sidebar2 from '../image/sidebar2.png';
 import Sidebar3 from '../image/sidebar3.png';
