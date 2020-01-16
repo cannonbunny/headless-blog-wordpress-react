00fa4ef87352410678b7b45c57f14a50b12acebb
C Y Law <chevy.cy.law@gmail.com>
2020-01-15 19:15:57 -0800

    Revert back to template

--- src/page/Photo.js
+++ src/page/Photo.js
@@ -10,11 +10,15 @@ import Photo6 from '../image/photo6.jpg';
 import Photo7 from '../image/photo7.JPG';
 import Photo8 from '../image/photo8.jpg';
 import Photo9 from '../image/photo9.jpg';
-import Photo10 from '../image/photo10.jpg';
+import Photo10 from '../image/photo10.JPG';
 // import Photo11 from '../image/photo11.JPG';
 import Photo12 from '../image/photo12.JPG';
 import Photo13 from '../image/photo13.jpg';
 import Photo14 from '../image/photo14.jpg';
+import Photo15 from '../image/photo15.JPG';
+import Photo16 from '../image/photo16.JPG';
+import Photo17 from '../image/photo17.JPG';
+// import Photo18 from '../image/photo18.JPG';
 import './Photo.css';
 import Header from "../Header";
 
@@ -31,16 +35,19 @@ function Photo (){
             <img src={Photo1} class="photoBoothPic" padding="0" alt="logo" />
             <img src={Photo10} class="photoBoothPic" padding="0" alt="logo" />
             <img src={Photo5} class="photoBoothPic" padding="0" alt="logo" />
+            <img src={Photo15} class="photoBoothPic" padding="0" alt="logo" />
           </div>
           <div class="photoBoothColumn">
             <img src={Photo2} class="photoBoothPic" padding="0" alt="logo" />
             <img src={Photo13} class="photoBoothPic" padding="0" alt="logo" />
             <img src={Photo6} class="photoBoothPic" padding="0" alt="logo" />
+            <img src={Photo16} class="photoBoothPic" padding="0" alt="logo" />
             </div>
             <div class="photoBoothColumn">
             <img src={Photo3} class="photoBoothPic" padding="0" alt="logo" />
             <img src={Photo8} class="photoBoothPic" padding="0" alt="logo" />
             <img src={Photo7} class="photoBoothPic" padding="0" alt="logo" />
+            <img src={Photo17} class="photoBoothPic" padding="0" alt="logo" />
             </div>
             <div class="photoBoothColumn">
             <img src={Photo4} class="photoBoothPic" padding="0" alt="logo" />
